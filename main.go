6aa0afa7b8e9f535374742e45fc93ed532ec7af6
Owain Perry <owainperry@statestreet.com>
2019-04-11 12:20:56 +0100

    1.2 version

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "service b - version 1.1")
+		fmt.Fprintf(w, "service b - version 1.2")
 	})
 	http.ListenAndServe(":8080", nil)
 }
